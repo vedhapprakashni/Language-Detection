29ae49cea47436a79566e755e39b0171bb4560a8
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-10-02 19:21:48 +0530

    shifted files into correct dir

--- app/main.py
+++ app/main.py
@@ -28,3 +28,6 @@ def predict(payload: TextIn):
     language = predict_pipeline(payload.text)
     return {"language": language}
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
